e364b8b4672c4619778a708d043262838a4c1603
TechsavvYash <mittal.yash12@hotmail.com>
2021-07-18 14:36:19 +0530

    Initialized the project
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Server up and running!!");
+})
+
+app.listen(5000, ()=>{
+    console.log("Server started at http://localhost:5000");
+})
